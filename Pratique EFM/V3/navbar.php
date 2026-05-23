84c497f2097507c33075b57edeb902694788a9d5
Achraf <achraf.arajoum@gmail.com>
2026-05-23 11:16:36 +0100

    Create navbar.php

---
+++ Pratique EFM/V3/navbar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav d-flex flex-direction-row">
+                <li class="nav-item active">
+                    <a class="nav-link" href="ListeH.php">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ListeReservation.php">Liste Reservation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">se déconnecter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reservEnCours.php">Reservation En Cours</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html> 
