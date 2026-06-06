d7296a8368f4c642c06d11f7902b5062e2e48b58
Achraf <achraf.arajoum@gmail.com>
2026-06-06 10:28:24 +0100

    Create stockge.php

---
+++ Pratique EFM/V5/EX1/stockge.php
@@ -0,0 +1,8 @@
+<?php
+// Création de l'objet $pro (1 pt)
+$pro = new Professeur(2024001, 'Ahmed', 'Ali', '1992-02-02', 'permanent');
+
+// Ajout dans le tableau $ListPro (1 pt)
+$ListPro = [];
+$ListPro[] = $pro;
+?>
