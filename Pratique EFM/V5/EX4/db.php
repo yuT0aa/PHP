9d4019109baf95110297d10cf791c64d285e109b
Achraf Arajoum <achraf.arajoum@gmail.com>
2026-06-11 08:48:36 +0100

    Update db.php

--- Pratique EFM/V5/EX4/db.php
+++ Pratique EFM/V5/EX4/db.php
@@ -6,7 +6,6 @@
 
     try{
         $cnx=new PDO("mysql:host=$Srvr;dbname=$dbname",$login,$PW);
-        $cnx->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         die("Erreur:".$e->getMessage());
     }
